9b903e12acd21527931eb86b36d8bcb0eb660fd2
jackfrojpg <78164682+jackfrojpg@users.noreply.github.com>
2023-01-23 21:01:23 +0000

    Create roads.js

---
+++ roads.js
@@ -0,0 +1,1 @@
+var roads = {"type":"FeatureCollection","name":"roads" {"features": [{"type":"Feature", "properties":{"name":"test"}, "geometry":{"type":"LineString","coordinates":[ [0, 0], [100, -100] ] }]}
