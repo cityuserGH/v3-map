39cadcf1947f2d33258488e20391b7bc264ff6e7
jackfrojpg <jackfrojpg.rblx@gmail.com>
2023-01-24 14:00:00 +0000

    update

--- roads.js
+++ roads.js
@@ -1,1 +1,30 @@
-var roads = {"type":"FeatureCollection","name":"roads" {"features": [{"type":"Feature", "properties":{"name":"test"}, "geometry":{"type":"LineString","coordinates":[ [0, 0], [100, -100] ] }]}
+// COORDINATES ARE IN THE FORM (y,x) FOR SOME STRANGE REASON
+
+var roads = {
+    "type":"FeatureCollection",
+    "name":"roads",
+    "features": [
+        {"type":"Feature", "properties": { "name":"401" }, "geometry": {"type":"LineString", "coordinates": [
+            [7.375, -492.625],
+            [184.25, -471.75],
+            [261.25, -364.75],
+            [284.25, -364.75],
+            [304.5, -372],
+            [316.5, -372.25],
+            [322.75, -371],
+            [337.71875, -363.46875],
+            [355, -350.5],
+            [383, -328.625],
+            [386.625, -324.125],
+            [410.25, -265],
+            [418.625, -249.5],
+            [434.5, -235.75],
+            [604.75, -161.25],
+            [957.25, -161.25]
+        ] } },
+
+        {"type":"Feature", "properties": { "name":"FS98" }, "geometry": { "type":"LineString", "coordinates": [
+            [-450.5, 584.25]
+        ]}}
+    ]
+}
